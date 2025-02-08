789b96504e92085098e249a530c80ff991b2d9d0
Jeric Epon <jericepon@gmail.com>
2025-02-08 16:51:05 +0800

    Refactor Todo functionality: implement CRUD operations, user authorization, and replace deprecated request classes

--- app/Http/Controllers/API/TodoController.php
+++ app/Http/Controllers/API/TodoController.php
@@ -1,66 +1,70 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Todo\StoreUpdateTodoRequest;
 use App\Models\Todo;
-use App\Http\Requests\StoreTodoRequest;
-use App\Http\Requests\UpdateTodoRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return Todo::where('user_id', Auth::id())->get();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTodoRequest $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreUpdateTodoRequest $request)
+  {
+    $validated_data = $request->validated();
+    $todo = new Todo($validated_data);
+    $todo->user_id = Auth::id();
+    $todo->save($validated_data);
+    return response()->json($validated_data);
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Todo $todo)
-    {
-        //
+  /**
+   * Display the specified resource.
+   */
+  public function show(Todo $todo)
+  {
+    if (Auth::id() === $todo->user_id) {
+      return response()->json($todo);
+    } else {
+      return response()->json(['error' => 'Unauthorized'], 403);
     }
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Todo $todo)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(StoreUpdateTodoRequest $request, Todo $todo)
+  {
+    $validated_data = $request->validated();
+    $todo->update($validated_data);
+    return $request;
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTodoRequest $request, Todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Todo $todo)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Todo $todo)
+  {
+    $todo->delete();
+    return response()->json(['message' => 'Todo deleted successfully']);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,4 +11,11 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
   Route::post('/logout', [AuthController::class, 'logout']);
   Route::get('/user', [AuthController::class, 'user']);
   Route::get('/me', [AuthController::class, 'me']);
+  Route::group(['todos'], function () {
+    Route::get('getAllTodos/', [TodoController::class, 'index']);
+    Route::post('saveTodo/', [TodoController::class, 'store']);
+    Route::get('getTodo/{todo}', [TodoController::class, 'show']);
+    Route::put('updateTodo/{todo}', [TodoController::class, 'update']);
+    Route::delete('delete/{todo}', [TodoController::class, 'destroy']);
+  });
 });
